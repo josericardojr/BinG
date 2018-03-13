962e227feeb1a57aaf06565b13817b3ae5ac7ed6
laRoppe <nascimentol@outlook.com>
2018-03-13 00:45:31 -0300

    comments

--- LoadedXML.py
+++ LoadedXML.py
@@ -12,7 +12,9 @@ class LoadedXML:
             vertexs.append(Vertex(v.find('ID'), v.find('type'), v.find('label'), v.find('date'), v.find('attributes')))
 
         for v in vertexs:
+            #Felipe aqui eu acesso todos os dados por funções
             print('id: {0}\ntype: {1}\nlabel: {2}\ndate: {3}'.format(v.id(), v.type(), v.label(), v.date()))
+            # o attributes() retorna uma lista
             atr = v.attributes()
             aux = 0
             for a in atr:
@@ -20,7 +22,7 @@ class LoadedXML:
                 print('attributes{0} name: {1} value: {2}'.format(aux, a.name(), a.value()))
             print('_' * 10)
 
-
+#aqui eu passei o nome do xml e depois a pasta que ela está
 LoadedXML(fullpath('Lucas_2.xml', 'XML'))
 
 
