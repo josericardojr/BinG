7e07820346501412d47b49108e17a589208b35a9
laRoppe <nascimentol@outlook.com>
2018-03-21 11:57:31 -0300

    hash has been implemented

--- Project/LoadedXML.py
+++ Project/LoadedXML.py
@@ -7,10 +7,11 @@ class LoadedXML:
     def __init__(self, path):
         xml = loadxml(path)
         vertices = xml.findall('vertices/vertex')
-        vertex = []
+        vertex = {}
 
         for v in vertices:
-            vertex.append(Vertex(v.find('ID'), v.find('type'), v.find('label'), v.find('date'), v.find('attributes')))
+            vid = v.find('ID')
+            vertex[vid.text] = Vertex(vid, v.find('type'), v.find('label'), v.find('date'), v.find('attributes'))
 
         self.vvertex = vertex
 
@@ -35,21 +36,21 @@ xml = LoadedXML(fullpath('Lucas_2.xml', 'XML'))
 vert = xml.vertexs()
 ed = xml.edge()
 
-for v in vert:
-    # Felipe aqui eu acesso todos os dados por funções
-    #print('id: {0}\ntype: {1}\nlabel: {2}\ndate: {3}'.format(v.id(), v.type(), v.label(), v.date()))
+for k, v in vert.items():
+    #Felipe aqui eu acesso todos os dados por funções
+    print('id: {0}\ntype: {1}\nlabel: {2}\ndate: {3}'.format(v.id(), v.type(), v.label(), v.date()))
     # o attributes() retorna uma lista
     atr = v.attributes()
     aux = 0
     for a in atr:
         aux = aux + 1
-        #print('attributes{0} name: {1} value: {2}'.format(aux, a.name(), a.value()))
-    #print('_' * 10)
-
-
-for e in ed:
-    print('id: {0}\ntype: {1}\nlabel: {2}\nvalue: {3}\nsourceID: {4}\ntargetID: {5}'.format(e.id(), e.type(), e.label(), e.value(), e.sourceID(), e.targetID()))
+        print('attributes{0} name: {1} value: {2}'.format(aux, a.name(), a.value()))
     print('_' * 10)
+
+
+#for e in ed:
+    #print('id: {0}\ntype: {1}\nlabel: {2}\nvalue: {3}\nsourceID: {4}\ntargetID: {5}'.format(e.id(), e.type(), e.label(), e.value(), e.sourceID(), e.targetID()))
+    #print('_' * 10)
 
 
 
