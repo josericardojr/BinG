e166fb114826a215f1b4ba7a87c970e97e8ba664
LucasNascimento <nascimentol@outlook.com>
2018-12-04 15:42:42 -0200

    can use attributes in target

--- FactGenerator/Schema/SchemaFact.py
+++ FactGenerator/Schema/SchemaFact.py
@@ -8,11 +8,17 @@ class SchemaFact:
 
         att = fact.find('attrib').attrib
 
-        self.att_name = att['name']
+        self.att_name = att['target']
+
+        attribute_key_spliter = '//'
 
         if 'filter' in att:
             self.att_filter = att['filter']
-            self.checkFact = lambda vertex: vertex.me.find(self.att_name).text == self.att_filter
+            if attribute_key_spliter in self.att_name:
+                splited = str(self.att_name).split(attribute_key_spliter)
+                self.checkFact = lambda vertex: self.tst(vertex, splited[len(splited) - 1])
+            else:
+                self.checkFact = lambda vertex: vertex.me.find(self.att_name).text == self.att_filter
         else:
             self.att_filter = ''
             self.checkFact = lambda vertex: True
@@ -21,3 +27,10 @@ class SchemaFact:
             self.att_value = att['value']
         else:
             self.att_value = self.att_name
+
+    def tst(self, v, key):
+        for att in v.me.find('attributes'):
+            if key in str(att.find('name').text):
+                if att.find('value').text == self.att_filter:
+                    return True
+        return False
