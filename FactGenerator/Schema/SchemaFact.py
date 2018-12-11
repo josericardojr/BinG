f90e5df601a84e8a7c7cd1884f5ba51466d22107
LucasMarcio <nascimentol@outlook.com>
2018-12-11 19:37:20 -0200

    added use with array attribute

--- FactGenerator/Schema/SchemaFact.py
+++ FactGenerator/Schema/SchemaFact.py
@@ -4,37 +4,44 @@ from xml.etree import ElementTree
 class SchemaFact:
     def __init__(self, fact):
 
+        attribute_key_spliter = '//'
         self.name = fact.attrib['name']
 
-        att = fact.find('attrib').attrib
+        info = fact.find('info').attrib
 
-        self.att_target = att['target']
+        targets = fact.find('attributes').findall('attrib')
+        count = -1
+        self.att_targets = []
+        self.att_filter = []
+        self.checkFacts = []
 
-        attribute_key_spliter = '//'
 
-        if 'filter' in att:
-            self.att_filter = att['filter']
-            if attribute_key_spliter in self.att_target:
-                splited = str(self.att_target).split(attribute_key_spliter)
-                self.checkFact = lambda vertex: self.get_attribute(vertex, splited[len(splited) - 1])
+        for t in targets:
+            if 'filter' in t.attrib:
+                count += 1
+                self.att_filter.append(t.attrib['filter'])
+                self.att_targets.append(t.attrib['target'])
+                if attribute_key_spliter in self.att_targets[count]:
+                    splited = str(self.att_targets[count]).split(attribute_key_spliter)
+                    self.checkFacts.append(lambda vertex: self.get_attribute(vertex, splited[len(splited) - 1], count))
+                else:
+                    self.checkFacts.append(lambda vertex: vertex.me.find(self.att_targets[count]).text == self.att_filter[count])
             else:
-                self.checkFact = lambda vertex: vertex.me.find(self.att_target).text == self.att_filter
-        else:
-            self.att_filter = ''
-            self.checkFact = lambda vertex: True
+                self.att_filter.append('empty')
+                self.checkFacts.append(lambda vertex: True)
 
-        if 'value' in att:
-            if attribute_key_spliter in att['value']:
-                splited = str(att['value']).split(attribute_key_spliter)
+        if 'value' in info:
+            if attribute_key_spliter in info['value']:
+                splited = str(info['value']).split(attribute_key_spliter)
                 self.att_value = splited[len(splited) - 1]
                 self.get_value = lambda vertex: self.get_text_attribute(vertex, self.att_value)
 
             else:
-                self.att_value = att['value']
+                self.att_value = info['value']
                 self.get_value = lambda vertex: self.get_text_standard(vertex, self.att_value)
         else:
-            self.att_value = self.att_target
-            self.get_value = lambda vertex: self.get_text_standard(vertex, self.att_value)
+            self.att_value = 'without value'
+            self.get_value = lambda vertex: False
 
     def get_text_standard(self, vertex, tag):
         text = 'didn\'t find'
@@ -63,9 +70,22 @@ class SchemaFact:
 
         return text
 
-    def get_attribute(self, v, key):
+    def checkFact(self, v):
+        count = -1
+        for target in self.att_targets:
+            count += 1
+            if not self.checkFacts[count](v):
+                return False
+
+        return True
+
+    def get_attribute(self, v, key, count):
+        count-=1
         for att in v.me.find('attributes'):
             if key in str(att.find('name').text):
-                if att.find('value').text == self.att_filter:
+                if att.find('value').text == self.att_filter[count]:
                     return True
+                #else:
+                    #print(v.me.find('ID').text)
+        #print('did not find: ' + key + ' <> ' + self.att_filter[count] + ' index: ' + str(count))
         return False
