061b61269d924475de0ee5006e3cfd1879a8b7c9
LucasNascimento <nascimentol@outlook.com>
2019-03-28 15:18:07 -0300

    cleaning classes : Prolog Rule Data

--- Prolog/PrologRuleData.py
+++ Prolog/PrologRuleData.py
@@ -9,37 +9,6 @@ class PrologRuleData:
         self.rules = []
 
     def setup_rule(self, rule):
-        s = rule.rule_name + '('
-        #print(s)
-
-        length = len(rule.inputs)
-        i = 0
-        for inp in rule.inputs:
-            s += rule.inputs[inp]
-            i += 1
-            if i < length:
-                s += ','
-
-        s += '):-\n\t'
-
-        count = len(rule.facts)
-        for fact in rule.facts:
-            length = len(rule.facts[fact])
-            s += fact + '('
-
-            for inp in rule.facts[fact]:
-                s += inp
-                length -= 1
-
-                if length > 0:
-                    s += ','
-
-            count -= 1
-            if count > 0:
-                s += '),\n\t'
-
-        s += ').'
-        #print(s)
         self.rules.append(rule)
 
     def get_rule(self, index):
