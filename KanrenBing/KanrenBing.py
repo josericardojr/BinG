4a3b7fc0f55a037276e86515f0e0e9d1b0f1f089
LucasNascimento <nascimentol@outlook.com>
2019-03-28 15:04:07 -0300

    cleaning classes : kanrenBing

--- KanrenBing/KanrenBing.py
+++ KanrenBing/KanrenBing.py
@@ -13,7 +13,6 @@ class KanrenBing:
             if f.get_fact()[0] not in bing_facts:
                 bing_facts[f.get_fact()[0]] = Relation()
             facts(bing_facts[f.get_fact()[0]], (f.get_fact()[1], f.get_fact()[2]))
-            #print(f.get_fact()[0] +'<>' +f.get_fact()[1]+'<>' + f.get_fact()[2])
 
         for rule in self.consult.rule_data.rules:
             exist = True
@@ -21,7 +20,7 @@ class KanrenBing:
             for f in rule.facts:
                 if f not in bing_facts:
                     exist = False
-                    #print('The Rule ' + rule.rule_name + '\'s fact: \'' + f + '\' dont exists')
+                    KanrenBing.error_rule(rule.rule_name, f)
                     break
 
             if exist:
@@ -29,27 +28,8 @@ class KanrenBing:
                     bing_rules[rule.rule_name] = KanrenBingRule(rule.get_request_input())
                     bing_rules[rule.rule_name].setup_rule(rule.facts, bing_facts)
 
-        '''x = var()
-        y = var()
-        s = var()
-
-        r1 = (x,s)
-
-        quest1 = (bing_facts['BeingHit_Player'](x, s))
-        quest2 = (bing_facts['BeingHit_Enemy'](y, s))
-        c = conde((quest1, quest2))
-
-        result = (run(0, s, c))
-
-        if len(result) > 1:
-            print('Results ' + str(len(result)) + ':')
-            for r in result:
-                print(r)
-        else:
-            print('Result: ' + str(result))
-
-        print('_____')
         for r_name in bing_rules:
+            print('/\\' * 3)
             print('Using rule: ' + r_name)
             result = (run(0, bing_rules[r_name].request_obj, bing_rules[r_name].get_request_command()))
 
@@ -58,4 +38,8 @@ class KanrenBing:
                 for re in result:
                     print(re)
             else:
-                print('Result: ' + str(result))'''
+                print('Result: ' + str(result))
+
+    @staticmethod
+    def error_rule(rule_name, f):
+        print('The Rule ' + rule_name + '\'s fact: \'' + f + '\' dont exists')
